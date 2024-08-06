070e03f7bc82053eaeace5ace58f306de8a522e7
Jeffrey Varner <jdv27@cornell.edu>
2024-08-06 07:19:27 -0400

    working on week 2 activities
    week 2

---
+++ lectures/week-2/L2a/Include.jl
@@ -0,0 +1,36 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
+
+# make sure all is up to date -
+using Pkg
+Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+import Pkg; Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update()
+
+# load external packages -
+using VLQuantitativeFinancePackage
+using DataFrames
+using CSV
+using Dates
+using LinearAlgebra
+using Statistics
+using StatsBase
+using Plots
+using Colors
+using StatsPlots
+using JLD2
+using FileIO
+using Distributions
+using LsqFit
+using MathOptInterface
+using DataStructures
+using PrettyTables
+using Flux
+using OneHotArrays
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Utility.jl"));
+
