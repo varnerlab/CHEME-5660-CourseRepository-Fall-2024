f33d514dac39ae67820c73c45970b87cea3ef0b8
Jeffrey Varner <jdv27@cornell.edu>
2024-09-03 08:52:43 -0400

    updated the Include.jl file
    Updated the Include.jl file

--- lectures/week-2/L2b/Include.jl
+++ lectures/week-2/L2b/Include.jl
@@ -6,8 +6,13 @@ const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
 
 # make sure all is up to date -
 using Pkg
-Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
-import Pkg; Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update()
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+# import Pkg; Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update()
 
 # load external packages -
 using VLQuantitativeFinancePackage
