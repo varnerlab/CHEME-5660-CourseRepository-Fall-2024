50c9b022914a51b1aad89e756e17d86f99b791f8
Jeffrey Varner <jdv27@cornell.edu>
2024-09-24 06:38:39 -0400

    bonus example
    bonus example

--- lectures/week-5/L5a/src/Files.jl
+++ lectures/week-5/L5a/src/Files.jl
@@ -3,3 +3,4 @@ function _jld2(path::String)::Dict{String,Any}
 end
 
 MyMarketDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2018-to-12-29-2023.jld2"));
+MyOutOfSampleMarketDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2024-to-09-24-2024.jld2"));
