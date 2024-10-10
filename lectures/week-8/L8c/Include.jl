f9177789a4923b252be096c43f1f8ac12cdfdb59
Jeffrey Varner <jdv27@cornell.edu>
2024-10-10 13:17:45 -0400

    working on real-time allocation
    real-time allocation

---
+++ lectures/week-8/L8c/Include.jl
@@ -0,0 +1,32 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# make sure all is up to date -
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using VLQuantitativeFinancePackage
+using DataFrames
+using CSV
+using Dates
+using LinearAlgebra
+using Statistics
+using Plots
+using Colors
+using StatsPlots
+using JLD2
+using FileIO
+using Distributions
+using MathOptInterface
+using DataStructures
+using PrettyTables
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
