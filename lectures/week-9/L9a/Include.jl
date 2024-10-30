5575c010b5c73c135b4fef096cdadf1705250192
Jeffrey Varner <jdv27@cornell.edu>
2024-10-30 06:07:51 -0700

    add fig save
    fig save

--- lectures/week-9/L9a/Include.jl
+++ lectures/week-9/L9a/Include.jl
@@ -2,6 +2,7 @@
 const _ROOT = @__DIR__;
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
 
 # make sure all is up to date -
 # using Pkg
