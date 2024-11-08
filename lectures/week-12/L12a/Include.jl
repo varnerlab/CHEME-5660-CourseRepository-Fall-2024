ca0298ebf196ec687033c3e27ae59f027ced731d
Jeffrey Varner <jdv27@cornell.edu>
2024-11-07 19:52:46 -0500

    example for T
    example for T

--- lectures/week-12/L12a/Include.jl
+++ lectures/week-12/L12a/Include.jl
@@ -5,8 +5,12 @@ const _PATH_TO_DATA = joinpath(_ROOT, "data");
 
 # download external packages
 import Pkg; 
-Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git");
-Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+# Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git");
+# Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 
 # load packages -
 using VLQuantitativeFinancePackage
@@ -22,4 +26,7 @@ using LinearAlgebra
 using StatsPlots
 using Distributions
 using HypothesisTests
-using PrettyTables
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Files.jl"))
+include(joinpath(_PATH_TO_SRC, "Search.jl"))
