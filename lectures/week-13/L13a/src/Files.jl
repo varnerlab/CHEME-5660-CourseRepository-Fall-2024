1f9e0d77745d6568dd9dadc815b8ccc40a1a7507
Jeffrey Varner <jdv27@cornell.edu>
2024-11-21 09:33:44 -0500

    Small changes
    Small changes

--- lectures/week-13/L13a/src/Files.jl
+++ lectures/week-13/L13a/src/Files.jl
@@ -3,4 +3,4 @@ function _jld2(path::String)::Dict{String,Any}
 end
 
 MyPortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2018-to-12-29-2023.jld2"));
-MyOutOfSamplePortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2024-to-09-24-2024.jld2"));
+MyOutOfSamplePortfolioDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2024-to-10-25-2024.jld2"));
