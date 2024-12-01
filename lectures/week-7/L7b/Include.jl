f42ea1e357a4abfd563a8a83374a18cc93ac69c6
Jeffrey Varner <jdv27@cornell.edu>
2024-12-01 05:56:29 -0500

    lecture
    lecture

--- lectures/week-7/L7b/Include.jl
+++ lectures/week-7/L7b/Include.jl
@@ -2,6 +2,7 @@
 const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_FIGURES = joinpath(_ROOT, "figs");
 
 # make sure all is up to date -
 using Pkg
