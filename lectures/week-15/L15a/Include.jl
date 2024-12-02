0dc857417515c3c6c110c68320a119ff900e09e7
Jeffrey Varner <jdv27@cornell.edu>
2024-12-02 17:46:50 -0500

    trader
    trader

--- lectures/week-15/L15a/Include.jl
+++ lectures/week-15/L15a/Include.jl
@@ -5,8 +5,10 @@ const _PATH_TO_DATA = joinpath(_ROOT, "data");
 
 # check: do we have the required packahes loaded??
 using Pkg
-Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
-Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
 
 # load external packages -
 using VLQuantitativeFinancePackage;
