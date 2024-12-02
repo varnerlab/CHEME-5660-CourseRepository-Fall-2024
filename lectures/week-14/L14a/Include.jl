53a4b66280e101e7da49c0fde92e1d8e38f1d109
Jeffrey Varner <jdv27@cornell.edu>
2024-12-02 06:34:22 -0500

    Frames
    Frames

--- lectures/week-14/L14a/Include.jl
+++ lectures/week-14/L14a/Include.jl
@@ -2,6 +2,9 @@
 const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_FIGURES = joinpath(_ROOT, "figs");
+const _PATH_TO_FRAMES = joinpath(_ROOT, "frames");
+
 
 # check: do we have the required packahes loaded??
 # using Pkg
