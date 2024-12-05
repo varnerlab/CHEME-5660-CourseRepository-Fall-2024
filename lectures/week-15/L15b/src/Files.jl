7879975c8d3290dddcf44772d17317a56b3f22fc
Jeffrey Varner <jdv27@cornell.edu>
2024-12-05 08:37:47 -0500

    Examples
    Examples

--- lectures/week-15/L15b/src/Files.jl
+++ lectures/week-15/L15b/src/Files.jl
@@ -2,4 +2,4 @@ function _jld2(path::String)::Dict{String,Any}
     return load(path);
 end
 
-MyOutOfSamepleMarketDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2024-to-10-25-2024.jld2"));
+MyOutOfSamepleMarketDataSet() = _jld2(joinpath(_PATH_TO_DATA, "SP500-Daily-OHLC-1-3-2024-to-12-04-2024.jld2"));
